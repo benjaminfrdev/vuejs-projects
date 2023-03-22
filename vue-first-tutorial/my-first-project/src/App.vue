26e79792302652f45cc796a71ce17222157120fc
khoa.vominh <khoa.vominh@nttdata.com>
2023-03-22 18:29:15 +0700

    new code

---
+++ vue-first-tutorial/my-first-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1 v-bind:title="message">{{ message }}</h1>
+    <button v-on:click="changeMessage">reverse message</button>
+    <input type="text" v-model="message" placeholder="Current Message value">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'first message invoke'
+    }
+  },
+  methods: {
+    changeMessage: function () {
+      this.message = this.message.split(' ').reverse().join(' ')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
