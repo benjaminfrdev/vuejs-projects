cefd347eef707fcc284b45ec4d6d02a84ad61903
khoa.vominh <khoa.vominh@nttdata.com>
2023-04-12 18:04:55 +0700

    udemy course

---
+++ vue-first-tutorial/my-app/src/components/Timeline.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs">
+            Today
+        </span>
+    </nav>
+</template>
