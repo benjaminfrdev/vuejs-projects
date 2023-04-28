7c74fb02a34110f57f6451485b9adc4c969db6ab
khoa.vominh <khoa.vominh@nttdata.com>
2023-04-28 17:35:59 +0700

    new code

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -1,10 +1,20 @@
 <template>
     <header class="w-full text-sm">
-        <div class="fixed top-0 left-0 w-full h-16 bg-white">
+        <div class="fixed top-0 left-0 w-full h-16 bg-white font-bold" >
             <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-                <a href="/" class="flex h-full items-center text-xl font-bold">{{company}}</a>
-                <h2 class="ml-8 flex h-full items-center font-bold"> Developed by {{author.firstName}}</h2>
+                <a :href="url" class="flex h-full items-center text-xl font-bold">{{company}}</a>
+                <nav class="ml-12 h-full" v-for="item of menuItems">
+                    <ul class="flex h-full list-none">
+                        <li class="h-full ml-9">
+                            <a href="" class="flex h-full items-center py-2.5">
+                                {{ item }}
+                            </a>
+                        </li>
+                    </ul>
+
+                </nav>
             </div>
+
         </div>  
     </header>
 
@@ -21,7 +31,16 @@ export default {
             firstName: "Benjamin",
             lastName: "Vo"
 
-        }
+        },
+        url: "https://careers.google.com",
+        menuItems: [
+            "Teams",
+            "Location",
+            "Life at Ben Corp",
+            "How We hire",
+            "Students",
+            "Jobs"
+        ]
     }
   }
 }
---
+++ job-search/tests/unit/components/MainNav.test.js
@@ -0,0 +1,5 @@
+import {
+    render, screen
+} from "@testing-library/vue"
+
+import MainNav from "@/components/MainNav.vue";
