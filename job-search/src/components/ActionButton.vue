f7461139f6d97312a3a08391e6709f7a0125d060
vo minh khoa <Benjamin.dev98@gmail.com>
2023-05-27 16:48:09 +0700

    new code @apply taiwind

--- job-search/src/components/ActionButton.vue
+++ job-search/src/components/ActionButton.vue
@@ -1,13 +1,22 @@
 <template>
-  <button
-    class="rounded border-0 px-5 py-3 font-medium bg-brand-blue-1 text-white hover:shadow-blue"
-  >
-    Sign in
+  <button class="primary">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text"],
 };
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+</style>
--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -1,15 +1,15 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white font-bold">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white font-bold">
       <div
-        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8"
+        class="flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
         <a :href="url" class="flex h-full items-center text-xl font-bold">{{
           company
         }}</a>
-        <nav class="ml-12 h-full" v-for="item of menuItems">
+        <nav v-for="(item, i) of menuItems" :key="i" class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li class="h-full ml-9">
+            <li class="ml-9 h-full">
               <a href="" class="flex h-full items-center py-2.5">
                 {{ item }}
               </a>
@@ -17,8 +17,8 @@
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="isLoggedIn" />
-          <action-button v-else @:click="loginUser" />
+          <ProfileImage v-if="isLoggedIn" />
+          <ActionButton v-else text="Sign In" @:click="loginUser" />
         </div>
       </div>
     </div>
@@ -35,12 +35,11 @@ export default {
     ActionButton,
     ProfileImage,
   },
-  methods: {},
   setup(props) {
     let isLoggedIn = ref(false);
     const loginUser = () => {
-        isLoggedIn.value = true
-    }
+      isLoggedIn.value = true;
+    };
     return {
       company: "Developer Careers",
       author: {
@@ -57,9 +56,10 @@ export default {
         "Jobs",
       ],
       isLoggedIn,
-      loginUser
+      loginUser,
     };
   },
+  methods: {},
 };
 </script>
 
